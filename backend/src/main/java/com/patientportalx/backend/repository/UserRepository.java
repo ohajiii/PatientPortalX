95d97985a2334d39873563892ddd6c50731cdb42
ohajiii <o.hajii2001@gmail.com>
2025-09-22 15:31:35 +0200

    feat(repository): add UserRepository with query methods

--- backend/src/main/java/com/patientportalx/backend/repository/UserRepository.java
+++ backend/src/main/java/com/patientportalx/backend/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.patientportalx.backend.repository;
 
-public class UserRepository {
+import com.patientportalx.backend.entity.User;
+import com.patientportalx.backend.entity.enums.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    List<User> findByRole(Role role);
 }
