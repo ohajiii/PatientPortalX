623b148a854805be999158c1663a08f86182ed00
ohajiii <o.hajii2001@gmail.com>
2025-09-22 15:58:38 +0200

    feat(repository): add NotificationRepository with patient and staff queries

--- backend/src/main/java/com/patientportalx/backend/repository/NotificationRepository.java
+++ backend/src/main/java/com/patientportalx/backend/repository/NotificationRepository.java
@@ -1,4 +1,12 @@
 package com.patientportalx.backend.repository;
 
-public class NotificationRepository {
+import com.patientportalx.backend.entity.Notification;
+import com.patientportalx.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByPatient(User patient);
+    List<Notification> findByStaff(User staff);
 }
